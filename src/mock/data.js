21c58efad030dbdd507818a26444259f5401f46e
se-bayan-elian <bezo2024@gmail.com>
2023-08-11 22:56:17 +0300

    setup mocks

---
+++ src/mock/data.js
@@ -0,0 +1,53 @@
+export const socialIcons = [
+  { icon: '/assets/images/google-icon.svg' },
+  { icon: '/assets/images/twitter-icon.svg' },
+  { icon: '/assets/images/linkedin-icon.svg' },
+  { icon: '/assets/images/github-icon.svg' },
+];
+
+export const newGames = [
+  {
+    name: 'dragon',
+    bg: '/assets/images/dragon-bg.png',
+    hero: '/assets/images/dragon.png',
+    title: '/assets/images/dragon-title.png',
+    text: 'Join in the new DLC with Kratos to learn more about him and his future.',
+  },
+  {
+    name: 'superman',
+    bg: '/assets/images/superman-bg.jpg',
+    hero: '/assets/images/superman.png',
+    title: '',
+    text: 'Be part of the Suicide Squad and kill the Justice League!-Amanda Waller',
+  },
+  {
+    name: 'spiderman',
+    bg: '/assets/images/spiderman-bg.jpg',
+    hero: '/assets/images/spiderman.png',
+    // title: '/assets/images/spiderman-title.png',
+    text: 'Join in the new DLC with Kratos toMiles Morales discovers powers from his mentor, Peter Parker. Master his unique, bio-electric venom blast attacks.',
+  },
+];
+
+export const lastPlayed = [
+  {
+    img: '/assets/images/lastPlayed-1.jpg',
+    text: 'Hogwarts Legacy 50%',
+    color: 'sky_blue',
+  },
+  {
+    img: '/assets/images/lastPlayed-2.jpg',
+    text: 'God Of War: Ragnarök 72.5%',
+    color: 'blue',
+  },
+  {
+    img: '/assets/images/last-played-3.jpg',
+    text: 'Crash Bandicoot N. Sane Trilogy 34%',
+    color: 'green',
+  },
+  {
+    img: '/assets/images/last-played-4.jpg',
+    text: 'Dying Light 2 Stay Human 100%',
+    color: 'yellow',
+  },
+];
